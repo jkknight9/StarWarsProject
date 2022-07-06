b5fe492cf8f519bed3d5295609b76590a82840fe
Jack Knight <jack.knight@canopytax.com>
2022-07-05 18:07:30 -0600

    Initial Commit

---
+++ Shared/StarWarsProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsProjectApp.swift
+//  Shared
+//
+//  Created by Jack Knight on 7/5/22.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
